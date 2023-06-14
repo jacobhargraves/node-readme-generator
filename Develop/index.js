637d0a45ead1ff36f9b1ceb51ba4fb780afde96a
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-06-14 10:39:54 -0600

    user input is now inserted into new file

--- Develop/index.js
+++ Develop/index.js
@@ -2,61 +2,57 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [        
+    {
+        type: 'input',
+        message: 'Enter the project title:',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter a description of the README:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions:',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Enter usage instructions:',
+        name: 'usage',
+    },
+    {
+        type: 'checkbox',
+        message: 'What license would you like to select?',
+        name: 'license',
+        choices: ['MIT', 'Creative Commons Attribution 4.0', 'Educational Community License v2.0', 'Open Software License 3.0' ]
+    },];
+
+// gets user input to create README contents
+function userInput() {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        console.log(answers);
+        writeToFile('newREADME.md', answers);
+    })
+    .catch((error) => {
+    if (error.isTtyError) {
+        console.log(error);
+    } 
+    });
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    // assigns fileName
-    fileName = 'newREADME.md'
-    // gets user input to create README contents
-    function userInput() {
-        inquirer
-            .prompt([
-                {
-                    type: 'input',
-                    message: 'Enter the project title:',
-                    name: 'title',
-                },
-                {
-                    type: 'input',
-                    message: 'Enter a description of the README:',
-                    name: 'description',
-                },
-                {
-                    type: 'input',
-                    message: 'Enter installation instructions:',
-                    name: 'installation',
-                },
-                {
-                    type: 'input',
-                    message: 'Enter usage instructions:',
-                    name: 'usage',
-                },
-                {
-                    type: 'checkbox',
-                    message: 'What license would you like to select?',
-                    name: 'license',
-                    choices: ['MIT', 'Creative Commons Attribution 4.0', 'Educational Community License v2.0', 'Open Software License 3.0' ]
-                },
-            ])
-            .then((answers) => {
-            console.log(answers);
-            })
-            .catch((error) => {
-            if (error.isTtyError) {
-                console.log(error);
-            } 
-            });
-    }
-
-        data =  
-
-    fs.appendFile(fileName, 'data', (err) => err ? console.error(err) : console.log('Commit logged!'));
+    fs.writeFile(fileName, JSON.stringify(data, null, 2), (err) => err ? console.error(err) : console.log('Commit logged!'));
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    writeToFile();
+    userInput();
 }
 
 // Function call to initialize app
