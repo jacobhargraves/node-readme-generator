8a7ca5936f8a388e8debf54393e5673d5120bbaf
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-06-14 09:50:37 -0600

    fs used to create README file

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,18 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile('README.md', 'data', (err) => err ? console.error(err) : console.log('Commit logged!'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile();
+}
 
 // Function call to initialize app
 init();
